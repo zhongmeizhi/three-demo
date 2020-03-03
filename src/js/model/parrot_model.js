e5b4db578291e7bfa815468e479d76d38c7fcf0f
zhongmeizhi <330447171@qq.com>
2020-03-03 18:32:11 +0800

    大鹏展翅 动画效果

--- src/js/model/parrot_model.js
+++ src/js/model/parrot_model.js
@@ -1,10 +1,14 @@
-
+// import * as THREE from 'three'
+import {
+	Clock, AnimationMixer
+} from 'three';
 import parrotGlb from '@assets/glb/Parrot.glb';
 import { GLTFLoader } from "@/js/loaders/GLTFLoader.js";
 
 class Parrot {
 	constructor(main) {
 		this.props = main;
+		this.clock = new Clock();
 	}
 	load() {
 		var loader = new GLTFLoader();
@@ -12,9 +16,16 @@ class Parrot {
 			const model = result.scene;
 			model.position.set(0, 10, -80);
 			this.props.scene.add(model);
+
+			result.scene.traverse( function ( child ) {
+				if ( child.isSkinnedMesh ) child.castShadow = true;
+			} );
+			this.mixer = new AnimationMixer( result.scene );
+			this.mixer.clipAction( result.animations[ 0 ] ).play();
 		});
 	}
 	animate() {
+		if ( this.mixer ) this.mixer.update( this.clock.getDelta() );
 	}
 	init() {
 		this.load();
